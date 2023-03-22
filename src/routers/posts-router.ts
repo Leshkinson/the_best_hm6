53248033652622ca80f6705329898e5a10e4d94b
Dmitri Aleks <alexeyonok.dmitriy@neatsoft.io>
2023-03-22 14:36:23 +0300

    corrected middleware

--- src/routers/posts-router.ts
+++ src/routers/posts-router.ts
@@ -5,6 +5,7 @@ import {authorizationGuard} from "../middleware/authorization-guard";
 import {postController} from "../controllers/post-controller";
 import {postValidations} from "../validators/post-validation";
 import {commentValidation} from "../validators/comment-validation";
+import {authMiddleware} from "../middleware/authMiddleware";
 
 
 export const postsRouter = Router({})
@@ -12,10 +13,10 @@ export const postsRouter = Router({})
 //-------------------GET---------------//
 postsRouter.get('/', postController.getAllPost)
 postsRouter.get('/:id', postController.getPostById)
-postsRouter.get('/:id/comments', postController.getPostComments)
+postsRouter.get('/:id/comments', checkPostId, postController.getPostComments)
 //-------------------POST---------------//
 postsRouter.post('/', authorizationGuard, postValidations, inputValidationMiddleware, postController.createPost)
-postsRouter.post('/:id/comments', checkPostId, commentValidation, inputValidationMiddleware, postController.createdComment)
+postsRouter.post('/:id/comments', authMiddleware, checkPostId, commentValidation, inputValidationMiddleware, postController.createdComment)
 //-------------------PUT---------------//
 postsRouter.put('/:id', authorizationGuard, checkPostId, postValidations, inputValidationMiddleware, postController.changePost)
 //-------------------DELETE---------------//
